25d6c1f0979d802c1494833633ca4e066dcb9437
Okoye <okoyedivine145@gmail.com>
2024-12-03 19:11:14 +0100

    Setup

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res
+    .status(200)
+    .json({ message: "Hope is the mind killer, hope is the last to die" });
+});
+
+app.listen(port, () => {
+  console.log(`App listening on http://localhost:${port}`);
+});
