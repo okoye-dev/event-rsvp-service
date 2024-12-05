4762de2c7cefecc7f57a719c169046a121f800a0
Okoye <okoyedivine145@gmail.com>
2024-12-05 10:16:03 +0100

    gitignore update

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,11 @@
-import express from "express";
-import "dotenv/config";
+import express from 'express';
+import 'dotenv/config';
 const app = express();
 const port = process.env.PORT;
-app.get("/", (req, res) => {
-    res
-        .status(200)
-        .json({ message: "Hope is the mind killer, hope is the last to die" });
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Hope is the mind killer, hope is the last to die',
+    });
 });
 app.listen(port, () => {
     console.log(`App listening on http://localhost:${port}`);
